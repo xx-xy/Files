74a1628b52637f952b197fb4a59121747df145f2
xx-xy <78575037+xx-xy@users.noreply.github.com>
2023-05-08 22:02:24 +0800

    Update fileball.js

--- fileball.js
+++ fileball.js
@@ -12,8 +12,8 @@
  
 [rewrite_local]
 # ～ filebox解鎖會員權限（2022-02-18）@ddgksf2013
-^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/(\$RCAnonymousID\%)?(\w)+$ url script-echo-response https://raw.githubusercontent.com/xx-xy/Files/master/QX/JS/filebox.js
-^https?:\/\/api\.revenuecat\.com\/v\d\/receipts$ url script-echo-response https://raw.githubusercontent.com/xx-xy/Files/master/QX/JS/filebox.js
+^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/(\$RCAnonymousID\%)?(\w)+$ url script-echo-response https://raw.githubusercontent.com/xx-xy/Files/master/fileball.js
+^https?:\/\/api\.revenuecat\.com\/v\d\/receipts$ url script-echo-response https://raw.githubusercontent.com/xx-xy/Files/master/fileball.js
 [mitm] 
 hostname=api.revenuecat.com
 ***********************************/
